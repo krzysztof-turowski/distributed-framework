5233fcca04ba9e96dc12a5da20bc27a92615b22c
tomekb234 <56112903+tomekb234@users.noreply.github.com>
2022-01-04 10:29:09 +0100

    split graph building functions for Runner and Synchronizer

--- lib/lib_graph_weighted.go
+++ lib/lib_graph_weighted.go
@@ -6,7 +6,7 @@ import (
 	"math/rand"
 )
 
-func addSynchronizedWeightedConnection(vertices []WeightedGraphNode, i, j, weight int) {
+func addWeightedConnection(vertices []WeightedGraphNode, i, j, weight int) {
 	chans := getSynchronousChannels(2)
 	addTwoWayWeightedConnection(
 		vertices[i].(*twoWayWeightedGraphNode), vertices[j].(*twoWayWeightedGraphNode),
@@ -16,8 +16,8 @@ func addSynchronizedWeightedConnection(vertices []WeightedGraphNode, i, j, weigh
 	log.Println("Channel", vertices[j].GetIndex(), "->", vertices[i].GetIndex(), "set up with weight ", weight)
 }
 
-func BuildSynchronizedEmptyWeightedGraph(n int, indexGenerator Generator) ([]WeightedGraphNode, Synchronizer) {
-	vertices, synchronizer := BuildSynchronizedEmptyGraph(n, indexGenerator)
+func BuildEmptyWeightedGraph(n int, indexGenerator Generator) ([]WeightedGraphNode, Runner) {
+	vertices, runner := BuildEmptyGraph(n, indexGenerator)
 	weightedVertices := make([]WeightedGraphNode, n)
 	for i := range weightedVertices {
 		weightedVertices[i] = &twoWayWeightedGraphNode{
@@ -25,16 +25,21 @@ func BuildSynchronizedEmptyWeightedGraph(n int, indexGenerator Generator) ([]Wei
 			weights: make([]int, 0),
 		}
 	}
-	return weightedVertices, synchronizer
+	return weightedVertices, runner
 }
 
-func BuildSynchronizedWeightedGraphFromAdjacencyList(adjacencyList [][][2]int, generator Generator) ([]WeightedGraphNode, Synchronizer) {
-	vertices, synchronizer := BuildSynchronizedEmptyWeightedGraph(len(adjacencyList), generator)
+func BuildSynchronizedEmptyWeightedGraph(n int, indexGenerator Generator) ([]WeightedGraphNode, Synchronizer) {
+	vertices, runner := BuildEmptyWeightedGraph(n, indexGenerator)
+	return vertices, asSynchronizer(runner)
+}
+
+func BuildWeightedGraphFromAdjacencyList(adjacencyList [][][2]int, generator Generator) ([]WeightedGraphNode, Runner) {
+	vertices, runner := BuildEmptyWeightedGraph(len(adjacencyList), generator)
 
 	for i, l := range adjacencyList {
 		for _, e := range l {
 			if j := e[0]; i < j-1 {
-				addSynchronizedWeightedConnection(vertices, i, j-1, e[1])
+				addWeightedConnection(vertices, i, j-1, e[1])
 			}
 		}
 	}
@@ -42,35 +47,45 @@ func BuildSynchronizedWeightedGraphFromAdjacencyList(adjacencyList [][][2]int, g
 	for _, vertex := range vertices {
 		vertex.(*twoWayWeightedGraphNode).shuffleTopology()
 	}
-	return vertices, synchronizer
+	return vertices, runner
 }
 
-func buildSynchronizedWeightedRandomTree(n, maxWeight int, generator Generator) ([]WeightedGraphNode, Synchronizer, map[[2]int]bool) {
-	vertices, synchronizer := BuildSynchronizedEmptyWeightedGraph(n, generator)
+func BuildSynchronizedWeightedGraphFromAdjacencyList(adjacencyList [][][2]int, generator Generator) ([]WeightedGraphNode, Synchronizer) {
+	vertices, runner := BuildWeightedGraphFromAdjacencyList(adjacencyList, generator)
+	return vertices, asSynchronizer(runner)
+}
+
+func buildWeightedRandomTree(n, maxWeight int, generator Generator) ([]WeightedGraphNode, Runner, map[[2]int]bool) {
+	vertices, runner := BuildEmptyWeightedGraph(n, generator)
 	edgesSet := make(map[[2]int]bool)
 	for j := 1; j < n; j++ {
 		i := rand.Intn(j)
-		addSynchronizedWeightedConnection(vertices, i, j, rand.Intn(maxWeight))
+		addWeightedConnection(vertices, i, j, rand.Intn(maxWeight))
 		edgesSet[[2]int{i, j}] = true
 		edgesSet[[2]int{j, i}] = true
 	}
-	return vertices, synchronizer, edgesSet
+	return vertices, runner, edgesSet
 }
 
-func BuildSynchronizedWeightedRandomTree(n, maxWeight int, generator Generator) ([]WeightedGraphNode, Synchronizer) {
-	vertices, synchronizer, _ := buildSynchronizedWeightedRandomTree(n, maxWeight, generator)
+func BuildWeightedRandomTree(n, maxWeight int, generator Generator) ([]WeightedGraphNode, Runner) {
+	vertices, runner, _ := buildWeightedRandomTree(n, maxWeight, generator)
 	for _, v := range vertices {
 		v.(*twoWayWeightedGraphNode).shuffleTopology()
 	}
-	return vertices, synchronizer
+	return vertices, runner
 }
 
-func BuildSynchronizedRandomConnectedWeightedGraph(n, m, maxWeight int, generator Generator) ([]WeightedGraphNode, Synchronizer) {
+func BuildSynchronizedWeightedRandomTree(n, maxWeight int, generator Generator) ([]WeightedGraphNode, Synchronizer) {
+	vertices, runner := BuildWeightedRandomTree(n, maxWeight, generator)
+	return vertices, asSynchronizer(runner)
+}
+
+func BuildRandomConnectedWeightedGraph(n, m, maxWeight int, generator Generator) ([]WeightedGraphNode, Runner) {
 	if maxM := n * (n - 1) / 2; m < n-1 || m > maxM {
 		panic(fmt.Sprintf("Number of edges should be within range [%d, %d]", n-1, maxM))
 	}
 
-	vertices, synchronizer, edgesSet := buildSynchronizedWeightedRandomTree(n, maxWeight, generator)
+	vertices, runner, edgesSet := buildWeightedRandomTree(n, maxWeight, generator)
 	possibleEdges := make([][2]int, 0)
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
@@ -84,7 +99,12 @@ func BuildSynchronizedRandomConnectedWeightedGraph(n, m, maxWeight int, generato
 	})
 	m = m - n + 1
 	for _, e := range possibleEdges[:m] {
-		addSynchronizedWeightedConnection(vertices, e[0], e[1], rand.Intn(maxWeight))
+		addWeightedConnection(vertices, e[0], e[1], rand.Intn(maxWeight))
 	}
-	return vertices, synchronizer
+	return vertices, runner
+}
+
+func BuildSynchronizedRandomConnectedWeightedGraph(n, m, maxWeight int, generator Generator) ([]WeightedGraphNode, Synchronizer) {
+	vertices, runner := BuildRandomConnectedWeightedGraph(n, m, maxWeight, generator)
+	return vertices, asSynchronizer(runner)
 }
