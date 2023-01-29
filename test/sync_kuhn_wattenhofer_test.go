f1f47c21634f82d67e5102fd6dd8c02b5770049e
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-01-29 23:01:24 +0100

    fix test package, imports

--- test/sync_kuhn_wattenhofer_test.go
+++ test/sync_kuhn_wattenhofer_test.go
@@ -1,36 +1,37 @@
-package sync_kuhn_wattenhofer
+package test
 
 import (
+	"github.com/krzysztof-turowski/distributed-framework/graphs/mds/sync_kuhn_wattenhofer"
 	"github.com/krzysztof-turowski/distributed-framework/lib"
 	"testing"
 )
 
 func TestDeltaVariantOnClique(t *testing.T) {
 	vertices, synchronizer := lib.BuildSynchronizedCompleteGraph(20)
-	RunUsingMaxDegreeInGraph(vertices, synchronizer, 6) // opt =1
+	sync_kuhn_wattenhofer.RunUsingMaxDegreeInGraph(vertices, synchronizer, 6) // opt =1
 }
 
 func TestLocalVariantOnClique(t *testing.T) {
 	vertices, synchronizer := lib.BuildSynchronizedCompleteGraph(20)
-	Run(vertices, synchronizer, 6) // opt = 1
+	sync_kuhn_wattenhofer.Run(vertices, synchronizer, 6) // opt = 1
 }
 
 func TestDeltaOnCycle(t *testing.T) {
 	vertices, synchronizer := lib.BuildSynchronizedRing(101)
-	RunUsingMaxDegreeInGraph(vertices, synchronizer, 2) // opt = 51
+	sync_kuhn_wattenhofer.RunUsingMaxDegreeInGraph(vertices, synchronizer, 2) // opt = 51
 }
 
 func TestLocalOnCycle(t *testing.T) {
 	vertices, synchronizer := lib.BuildSynchronizedRing(101)
-	Run(vertices, synchronizer, 2) // opt = 51
+	sync_kuhn_wattenhofer.Run(vertices, synchronizer, 2) // opt = 51
 }
 
 func TestDeltaRandom(t *testing.T) {
 	vertices, synchronizer := lib.BuildSynchronizedRandomGraph(101, 0.05)
-	RunUsingMaxDegreeInGraph(vertices, synchronizer, 4) // opt = ??
+	sync_kuhn_wattenhofer.RunUsingMaxDegreeInGraph(vertices, synchronizer, 4) // opt = ??
 }
 
 func TestLocalRandom(t *testing.T) {
 	vertices, synchronizer := lib.BuildSynchronizedRandomGraph(101, 0.05)
-	Run(vertices, synchronizer, 4) // opt = ??
+	sync_kuhn_wattenhofer.Run(vertices, synchronizer, 4) // opt = ??
 }
